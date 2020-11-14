a941f522fdfd532118b5090ce314ef3c6cbc06e6
Joyce Fernandes <joyce_f.s@hotmail.com>
2020-11-14 17:54:55 -0300

    Adiciona função de escutar evento das bolas

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const bolas = document.getElementsByClassName('ball');
+const textoCor = document.getElementById('rgb-color');
+const textoResposta = document.getElementById('answer');
+const botaoReiniciar = document.getElementById('reset-game');
+const cores = [];
 
-function numeroAleatorio() {
-    const numero = Math.floor(Math.random() * 256);
+function numeroAleatorio(mul) {
+    const numero = Math.floor(Math.random() * mul);
     return numero;    
 }
 
 function corAleatoria() {
-    const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;
+    const rgb = `rgb(${numeroAleatorio(256)}, ${numeroAleatorio(256)}, ${numeroAleatorio(256)})`;
     return rgb;
 }
 
-for (let bola = 0; bola < bolas.length; bola += 1) {
-    bolas[bola].style.backgroundColor = corAleatoria()
+function cliqueNaBola(event) {
+    const corSelecionada = event.target.style.backgroundColor;
+    if (corSelecionada === textoCor.textContent) {
+        textoResposta.textContent = 'Acertou!'
+    } else {
+        textoResposta.textContent = 'Errou! Tente novamente!';
+
+    }
 }
+
+for (let bola = 0; bola < bolas.length; bola += 1) {
+    const cor = corAleatoria();
+    bolas[bola].style.backgroundColor = cor;
+    cores[bola] = cor;
+    bolas[bola].addEventListener('click', cliqueNaBola); 
+}
+
+function adicionaCorAleatoria() {
+    textoCor.textContent = cores[numeroAleatorio(6)];
+}
+adicionaCorAleatoria();

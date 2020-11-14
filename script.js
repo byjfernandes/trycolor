e4533ba7a81a225cab2f42f0f3511ab43804b9ff
Joyce Fernandes <joyce_f.s@hotmail.com>
2020-11-14 16:51:01 -0300

    Corrige issue

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const bolas = document.getElementsByClassName('ball');
 function numeroAleatorio() {
     const numero = Math.floor(Math.random() * 256);
     return numero;    
-};
+}
 
 function corAleatoria() {
     const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;
     return rgb;
 }
 
-for (let bola = 0; bola < bolas.length; bola++) {
+for (let bola = 0; bola < bolas.length; bola += 1) {
     bolas[bola].style.backgroundColor = corAleatoria()
-};
+}

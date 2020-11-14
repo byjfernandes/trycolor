db6865c55820efdbe9721db373fce6bd0dad2ed8
Joyce Fernandes <joyce_f.s@hotmail.com>
2020-11-14 16:41:58 -0300

    Gera cores aleatorias e insere nos circulos

---
+++ script.js
@@ -0,0 +1,15 @@
+const bolas = document.getElementsByClassName('ball');
+
+function numeroAleatorio() {
+    const numero = Math.floor(Math.random() * 256);
+    return numero;    
+};
+
+function corAleatoria() {
+    const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;
+    return rgb;
+}
+
+for (let bola = 0; bola < bolas.length; bola++) {
+    bolas[bola].style.backgroundColor = corAleatoria()
+};
